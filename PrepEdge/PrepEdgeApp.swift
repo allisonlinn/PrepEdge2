9b0fd443588b9231421ab9d311602641356be91a
allisonlinn <allison.ytlin@gmail.com>
2023-07-16 12:28:10 -0400

    Initial Commit

---
+++ PrepEdge/PrepEdgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepEdgeApp.swift
+//  PrepEdge
+//
+//  Created by Allison Lin on 2023/7/16.
+//
+
+import SwiftUI
+
+@main
+struct PrepEdgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
